96947dbc4fbb7e7c8e47a8a780f117ab90e9420c
s3ryz0 <s3ryz000@gmail.com>
2025-05-30 23:15:41 +0800

    Update Player.java

--- RPG/Player.java
+++ RPG/Player.java
@@ -1,40 +1,17 @@
-public class Player{
-	private String name;
-	private int healthPts;
-	private int attackPts;
-	private Weapon equippedWeapon;
+public class Player extends Character {
+    private Weapon weapon;
 
-	Player(String name, int healthPts, int attackPts){
-		this.name = name;
-		this.healthPts = healthPts;
-		this.attackPts = attackPts;
-	}
+    public Player(String name, int baseHealth, int baseAtk, int baseDef, int level, Weapon weapon) {
+        super(name, baseHealth + (level * 10), baseAtk + (level * 2), baseDef + (level * 2), level);
+        this.weapon = weapon;
+        System.out.println("[LOG] Player constructor called");
+    }
 
-	public String getName(){
-		return name;
-	}
-	public int getHealthPts(){
-		return healthPts;
-	}
-	public int getAttackPts(){
-		return attackPts;
-	}
+    public int totalAttack() {
+        return getBaseAtk() + weapon.getWeaponDmg();
+    }
 
-	public String attack(){
-		attackPts += equippedWeapon.getAttackPts();
-		return "Rome attacks with " + attackPts + " damage!";
-	}
-
-	public String setEquip(Weapon weapon){
-		this.equippedWeapon = weapon;
-		return "You have equipped [" + weapon.getName() + "]\n" + weapon.toString();
-	}
-
-	@Override
-	public String toString(){
-		return "Player Name: " + name +
-				" | HealthPts: " + healthPts +
-				" | AttackPts: " + attackPts;
-	}
-	
+    public Weapon getWeapon() {
+        return weapon;
+    }
 }
